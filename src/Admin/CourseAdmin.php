988c3ca34757799b8c88d8fffc968c7d452fa5db
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2015-04-19 21:59:19 -0400

    Added CourseAdmin

---
+++ src/Admin/CourseAdmin.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Opfura package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Opfura\SchoolBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * CourseAdmin
+ *
+ * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+ * @since  0.4.0
+ */
+class CourseAdmin extends Admin
+{
+    /**
+     * Fields to be shown on create/edit forms
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.4.0
+     *
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Course Title'))
+        ;
+    }
+
+    /**
+     * Fields to be shown on filter forms
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.4.0
+     *
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+        ;
+    }
+
+
+    /**
+     * Fields to be shown on lists
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.4.0
+     *
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('createdAt')
+        ;
+    }
+}
