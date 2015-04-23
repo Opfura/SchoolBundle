e4d5b6cf758d058c04b0c84b2f3c37186390c338
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2015-04-22 22:05:46 -0400

    Added TeacherAdmin

---
+++ src/Admin/TeacherAdmin.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Opfura package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Opfura\SchoolBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * TeacherAdmin
+ *
+ * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+ * @since  0.6.0
+ */
+class TeacherAdmin extends Admin
+{
+    /**
+     * Fields to be shown on create/edit forms
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.6.0
+     *
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('user')
+        ;
+    }
+
+    /**
+     * Fields to be shown on filter forms
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.6.0
+     *
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('user')
+        ;
+    }
+
+
+    /**
+     * Fields to be shown on lists
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.6.0
+     *
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('user')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+}
