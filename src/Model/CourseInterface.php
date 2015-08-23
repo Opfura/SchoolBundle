dd1e10e7ea85a7aed58c45495ebf110cd149dc36
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2015-08-23 12:09:41 -0400

    Added CourseInterface

---
+++ src/Model/CourseInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Opfura package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Opfura\SchoolBundle\Model;
+
+/**
+ * CourseInterface
+ *
+ * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+ * @since  0.20.0
+ */
+interface CourseInterface
+{
+}
